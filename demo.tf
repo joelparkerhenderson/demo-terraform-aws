fbf932dba36965dff5f90dc38e6dd386c1717e5a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-02-18 23:12:03 -0800

    Upgrade instance block

--- demo.tf
+++ demo.tf
@@ -234,9 +234,59 @@ data "aws_ami" "ubuntu" {
   owners = [aws_ami_owner_id_of_canonical]
 }
 
-resource "aws_instance" "example" {
-  # For our EC2 instance, we specify an AMI for Ubuntu, 
-  # a "t2.micro" instance, so we can use the free tier.
-  ami = "ami-2757f631"
-  instance_type = "t2.micro"
+##
+#
+# Instances
+#
+##
+
+# Define local varibles that notate what the AWS free tier can do.
+locals {
+  free__aws_instance__instance_type = "t2.micro"
+  free__aws_instance__root_block_device__volume_size = "30"
+}
+
+# Create an EC2 instance, using our existing key pair from above.
+resource "aws_instance" "demo" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = local.free__aws_instance__instance_type
+  associate_public_ip_address = true
+  key_name = "administrator"
+  vpc_security_group_ids = [aws_security_group.demo_security_group.id]
+
+  tags = {
+    Name = "demo"
+  }
+
+  root_block_device {
+    volume_size           = local.free__aws_instance__root_block_device__volume_size
+  }
+
+  # Install some typical software, by using the typical package manager.
+  # You will likely want to customize this section for your own purposes.
+  user_data = <<EOF
+		#!/bin/sh
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install -y build-essential
+    sudo apt-get install -y curl
+    sudo apt-get install -y default-jdk
+    sudo apt-get install -y emacs
+    sudo apt-get install -y fd-find
+		sudo apt-get install -y git
+    sudo apt-get install -y git-core 
+		sudo apt-get install -y htop
+		sudo apt-get install -y jq
+    sudo apt-get install -y libssl-dev
+    sudo apt-get install -y libv8-dev
+		sudo apt-get install -y nginx
+    sudo apt-get install -y openssl
+    sudo apt-get install -y ripgrep
+ 		sudo apt-get install -y ruby
+    sudo apt-get install -y software-properties-common
+ 		sudo apt-get install -y tmux
+ 		sudo apt-get install -y vim
+    sudo apt-get install -y wget
+	EOF
+
 }
