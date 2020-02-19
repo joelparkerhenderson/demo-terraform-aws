3fc0090a22262a078df355c05e4763b4551c920c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-02-18 23:10:27 -0800

    Add identity extras

--- demo.tf
+++ demo.tf
@@ -71,6 +71,39 @@ resource "aws_iam_user" "xo" {
   }
 }
 
+##
+#
+# Identity etc.
+#
+##
+
+# Create a default group for our organization.
+# This is helpful because our AWS account also has
+# default groups for many of our partner organizations.
+resource "aws_iam_group" "nonprofitnetworks_default_group" {
+  name = "nonprofitnetworks_default_group"
+}
+
+# Create a default group policy for our organization.
+# We recommend including at least the polict to change password.
+# You will likely want to customize this for your purposes.
+resource "aws_iam_group_policy_attachment" "nonprofitnetworks_default_group_policy_attachment" {
+  group = aws_iam_group.nonprofitnetworks_default_group.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+# Use an existing key pair that we created via the the AWS console.
+# We named the key pair `administrator`, and we import it like this:
+#
+#     terraform import aws_key_pair.administrator administrator
+#
+# We use this key pair for our default needs, such as SSH to an instance.
+# Our public key below won't work for you, so you'll want to change it.
+resource "aws_key_pair" "administrator" {
+  key_name   = "administrator"
+  public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmXB4ltuJqH7xOUYOwkY9O4H4Wkd5+fDzhWbCeD3vnIYJB7j+V0M9/4b1wLXOZyZkyxvYkaxKdRy0Q41esWp7KaOHgmAHeIXZyXyXdKoofylDilxG1wRx0/b03scdnO5jNIof+Otp/8z57Y2xzr+pqbWGal6D/8VGLyykOKrGFGNft+2mOsAquOnKoZ1siIK44tkPt7D2LfQp+PrckKEQ5TSAvXTisRbQxF3VRJePf6cCADLYwShza8GKJrK+vUVo2GNJ9Pn4yvT9L2zUa0eYflHeq4799045meqY+Jn/Y+IOYN0dEDsWEv7sqMgjzmC1fm/Pp/hWjUDXXo++r0ir9wIDAQAB"
+}
+
 resource "aws_instance" "example" {
   # For our EC2 instance, we specify an AMI for Ubuntu, 
   # a "t2.micro" instance, so we can use the free tier.
