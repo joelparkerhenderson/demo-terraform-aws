0cb55600efedd99f2204eaff313e755d18f40fb9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-02-20 21:54:28 -0800

    Add more user data

--- demo.tf
+++ demo.tf
@@ -284,27 +284,44 @@ resource "aws_instance" "demo" {
   # You will likely want to customize this section for your own purposes.
   user_data = <<EOF
 		#!/bin/sh
-    sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get install -y build-essential
-    sudo apt-get install -y curl
-    sudo apt-get install -y default-jdk
-    sudo apt-get install -y emacs
-    sudo apt-get install -y fd-find
-		sudo apt-get install -y git
-    sudo apt-get install -y git-core 
-		sudo apt-get install -y htop
-		sudo apt-get install -y jq
-    sudo apt-get install -y libssl-dev
-    sudo apt-get install -y libv8-dev
-		sudo apt-get install -y nginx
-    sudo apt-get install -y openssl
-    sudo apt-get install -y ripgrep
- 		sudo apt-get install -y ruby
-    sudo apt-get install -y software-properties-common
- 		sudo apt-get install -y tmux
- 		sudo apt-get install -y vim
-    sudo apt-get install -y wget
+    sudo apt-get -q -y update
+    sudo apt-get -q -y upgrade
+
+    sudo apt-get install -q -y apt-transport-https
+    sudo apt-get install -q -y build-essential
+    sudo apt-get install -q -y ca-certificates
+    sudo apt-get install -q -y curl
+    sudo apt-get install -q -y gnupg-agent
+    sudo apt-get install -q -y software-properties-common
+
+    sudo apt-get install -q -y libssl-dev
+    sudo apt-get install -q -y libv8-dev
+
+    sudo apt-get install -q -y default-jdk
+    sudo apt-get install -q -y emacs
+    sudo apt-get install -q -y fd-find
+		sudo apt-get install -q -y git
+    sudo apt-get install -q -y git-core 
+		sudo apt-get install -q -y htop
+		sudo apt-get install -q -y jq
+		sudo apt-get install -q -y nginx
+    sudo apt-get install -q -y openssl
+    sudo apt-get install -q -y python3.6
+    sudo apt-get install -q -y ripgrep
+ 		sudo apt-get install -q -y ruby
+ 		sudo apt-get install -q -y tmux
+ 		sudo apt-get install -q -y vim
+    sudo apt-get install -q -y wget
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get -q -y update
+    sudo apt-get install -q -y docker-ce 
+    sudo apt-get install -q -y docker-ce-cli
+    sudo apt-get install -q -y containerd.io
+
+    sudo apt-get -q -y autoclean
+    sudo apt-get -q -y --purge autoremove
 	EOF
 
 }
