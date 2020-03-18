3437baaea003afbbbe78a18e9a7e25dea5c7ba31
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-03-18 14:22:52 -0700

    Refactor password policy to new file

---
+++ aws_iam_account_password_policy.tf
@@ -0,0 +1,20 @@
+##
+#
+# Password policy
+#
+# We like to configure a strong password policy.
+#
+# We expect our users to have a password manager.
+#
+##
+
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length = 32
+  require_lowercase_characters = true
+  require_numbers = true
+  require_uppercase_characters = true
+  require_symbols = true
+  allow_users_to_change_password = true
+  password_reuse_prevention = 20
+  max_password_age = 180
+}
